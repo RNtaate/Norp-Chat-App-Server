db2f872dd5c4b9483f5a56bd7436f32f65dbacae
RNtaate <royhyde14@gmail.com>
2022-05-24 14:35:36 +0300

    Set up a socket server using http

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
 
 const PORT = 3001;
 
-app.listen(PORT, () => {
+app.use(cors());
+
+const io = new Server(server, {
+  cors: {
+    options: ["http://localhost:3000"]
+  }
+})
+
+io.on('connection', (socket) => {
+  console.log("User has made a connection with the socket server");
+})
+
+server.listen(PORT, () => {
   console.log("Server is listening on port: " + PORT)
 })
